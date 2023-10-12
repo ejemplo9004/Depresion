e4473e1502ced3763768e0720bb26f58f71b1506
alejo <alejandro221app@gmail.com>
2023-10-12 13:33:01 -0500

    ContadorAlimentosFuncionando

--- Assets/_Game/Scripts/H3/Alimentos.cs
+++ Assets/_Game/Scripts/H3/Alimentos.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.Interaction.Toolkit;
 public class Alimentos : MonoBehaviour
 {
     
     public TipoAlimentos alimento;
     public bool enMano;
+    private XRGrabInteractable grabInteractable;
+
+    private void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.onSelectEntered.AddListener(OnGrabbed);
+        grabInteractable.onSelectExited.AddListener(OnReleased);
+    }
+
+    private void OnGrabbed(XRBaseInteractor interactor)
+    {
+        enMano = true;
+    }
+
+    private void OnReleased(XRBaseInteractor interactor)
+    {
+        enMano = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("ENTRANDO");
         Debug.Log(enMano );
+
         if (enMano == true && other.CompareTag("Canasta"))
         {
             Debug.Log("DENTRO");
             Canasta.canasta.ContadorAlimento(alimento);
+            Destroy(gameObject);
 
         }
     }
--- Assets/_Game/Scripts/H3/Canasta.cs
+++ Assets/_Game/Scripts/H3/Canasta.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Canasta : MonoBehaviour
 {
     public static Canasta canasta;
     public int cantidadBuenos;
     public int cantidadMalos;
+    public Text cantidadMalosTexto;
+    public Text cantidadBuenosTexto;
     private void Awake()
     {
         canasta = this;
@@ -18,9 +20,11 @@ public class Canasta : MonoBehaviour
         {
             case TipoAlimentos.ultraprocesado:
                 cantidadMalos++;
+                cantidadMalosTexto.text = cantidadMalos.ToString();
                 break;
             case TipoAlimentos.mediterraneo:
                 cantidadBuenos++;
+                cantidadBuenosTexto.text = cantidadBuenos.ToString();
                 break;
             default:
                 break;
